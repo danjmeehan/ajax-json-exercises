574483516a9bd5547b1836eab82a323a309d46a5
Dan Meehan <Dan Meehan>
2016-02-15 21:41:23 -0600

    Amazoon JSON, Exercises 1-3

--- exercise-2/exercise-2.js
+++ exercise-2/exercise-2.js
@@ -1,1 +1,17 @@
-// Write your code here!
+$(document).ready(function(){
+  var items = $.get('http://portal.batchacademy.com/api/wdfne/test-data/items', function(items){
+  	
+  	console.log(items);
+
+  var itemTemplate = $('template#item').html();
+
+  for(var i = 0; i < items.length; i++){
+
+  	var newItem = $(itemTemplate);
+  	$('.name',newItem).text(items[i].name);
+  	$('.price',newItem).text('$' + items[i].price.toFixed(2));
+  	$('.items').append(newItem);
+  }
+
+	});
+});
--- exercise-3/exercise-3.js
+++ exercise-3/exercise-3.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  var insert = $.get('http://portal.batchacademy.com/api/wdfne/test-data/hello-world');
+  var insert = $.get('http://portal.batchacademy.com/api/wdfne/test-data/hello-world', function(insert){
 
-  $('.dynamic').text(insert);
+  	$('.dynamic').text(insert);
+	});
 });
